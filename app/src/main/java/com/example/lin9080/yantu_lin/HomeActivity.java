fc5f84d302379d7c4b83a4385ac3fe4790a7106a
lin7 <908069808@qq.com>
2019-11-08 18:09:14 +0800

    add drawerlayout

--- app/src/main/java/com/example/lin9080/yantu_lin/HomeActivity.java
+++ app/src/main/java/com/example/lin9080/yantu_lin/HomeActivity.java
@@ -1,6 +1,11 @@
 package com.example.lin9080.yantu_lin;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -10,6 +15,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -17,10 +23,19 @@ import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout=(DrawerLayout)findViewById(R.id.main_drawerLayout);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_account_circle_black_24dp);
+        }
         /*
         String userid=getIntent().getStringExtra("userid");
         //TODO 根据userid初始化界面
@@ -30,7 +45,7 @@ public class HomeActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this,R.id.fragment);
         AppBarConfiguration configuration = new AppBarConfiguration.Builder(bottomNavigationView.getMenu()).build();
-        NavigationUI.setupActionBarWithNavController(this,navController,configuration);
+        //NavigationUI.setupActionBarWithNavController(this,navController,configuration);
         NavigationUI.setupWithNavController(bottomNavigationView,navController);
     }
 
@@ -46,5 +61,16 @@ public class HomeActivity extends AppCompatActivity {
         home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         home.addCategory(Intent.CATEGORY_HOME);
         startActivity(home);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+                default:
+        }
+        return true;
     }
 }
